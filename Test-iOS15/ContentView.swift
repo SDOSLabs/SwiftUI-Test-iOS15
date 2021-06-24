859e590ac168f85988ba00925d7cb1d3638f9267
Rafael Fernández <rafael.fernandez@sdos.es>
2021-06-24 13:30:04 +0200

    Initial Commit

---
+++ Test-iOS15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test-iOS15
+//
+//  Created by Rafael Fernandez Alvarez on 24/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test-iOS15/Test_iOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  Test_iOS15App.swift
+//  Test-iOS15
+//
+//  Created by Rafael Fernandez Alvarez on 24/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Test_iOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
